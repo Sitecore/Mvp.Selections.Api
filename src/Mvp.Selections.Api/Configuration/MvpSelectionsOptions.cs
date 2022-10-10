184b0977a2292b1ca8d57e4593ce12f6502c66f8
Ivan Lieckens <ivanlieckens@hotmail.com>
2022-10-10 17:14:06 +0200

    Contribution Time Frame Validation
    + Server side support for Sitecore/XM-Cloud-Introduction#149

---
+++ src/Mvp.Selections.Api/Configuration/MvpSelectionsOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Mvp.Selections.Api.Configuration
+{
+    public class MvpSelectionsOptions
+    {
+        public const string MvpSelections = "MvpSelections";
+
+        public int TimeFrameMonths { get; set; } = 18;
+    }
+}
