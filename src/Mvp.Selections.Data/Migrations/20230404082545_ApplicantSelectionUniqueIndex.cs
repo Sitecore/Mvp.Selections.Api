f1fc8400480c461c14aa2d26ea3bae109740c7ef
Ivan Lieckens <ivan.lieckens@sitecore.com>
2023-04-04 18:32:59 +0200

    Unique Index & Removed API Management (#43)
    + DB Unique Index on Applicant & Selection for Application
    + Removed API Management from infrastructure
    
    ---------
    
    Co-authored-by: Ivan Lieckens <ivanlieckens@hotmail.com>

---
+++ src/Mvp.Selections.Data/Migrations/20230404082545_ApplicantSelectionUniqueIndex.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mvp.Selections.Data.Migrations
+{
+    public partial class ApplicantSelectionUniqueIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Applications_ApplicantId",
+                table: "Applications");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_ApplicantId_SelectionId",
+                table: "Applications",
+                columns: new[] { "ApplicantId", "SelectionId" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Applications_ApplicantId_SelectionId",
+                table: "Applications");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_ApplicantId",
+                table: "Applications",
+                column: "ApplicantId");
+        }
+    }
+}
