fdff246dd77f33694822abc168b108f28d26fb88
Ivan Lieckens <ivanlieckens@hotmail.com>
2023-11-13 10:44:24 +0100

    Fix PATCH of Contribution
    + Improved AddRange to leverage concrete implementation if the underlying type is List<>
    + Extended deserialization to extract property keys of actual JSON message to leverage during PATCH evaluation
    + Fixed issue where creating a contribution with a date outside of the Selection would not return a message with the reason why

--- src/Mvp.Selections.Api/Services/Interfaces/IContributionService.cs
+++ src/Mvp.Selections.Api/Services/Interfaces/IContributionService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Mvp.Selections.Api.Model.Request;
 using Mvp.Selections.Domain;
@@ -11,6 +12,6 @@ namespace Mvp.Selections.Api.Services.Interfaces
 
         Task<OperationResult<Contribution>> RemoveAsync(User user, Guid applicationId, Guid id);
 
-        Task<OperationResult<Contribution>> UpdateAsync(User user, Guid id, Contribution contribution);
+        Task<OperationResult<Contribution>> UpdateAsync(User user, Guid id, Contribution contribution, IList<string> propertyKeys);
     }
 }
